4c739e49747a33d7ab40ed752ace5822ab3aa98b
Jameson.Brown <jameson.brown@mx.com>
2020-10-08 00:15:46 -0600

    Added filter modal and refactored featured eats

--- src/views/EatView.js
+++ src/views/EatView.js
@@ -1,42 +1,83 @@
 import React, { useEffect, useState } from 'react'
+import { css } from 'glamor'
+
 import { restaurants } from '../firebase'
 import { EatSummary } from '../components/eats/EatSummary'
+import { FilterModal } from '../components/FilterModal'
 
 export const EatView = () => {
   const [eats, setEats] = useState([])
+  const [featured, setFeatured] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [showFilter, toggleFilter] = useState(false)
+  const [filter, updateFilter] = useState('all')
   const styles = getStyles()
 
   useEffect(() => {
     let unsubscribe = restaurants((res) => {
       let list = []
+      let featuredList = []
 
-      res.forEach((eat) => (list = [...list, { id: eat.id, ...eat.data() }]))
+      res.forEach((eat) =>
+        eat.data().featured
+          ? (featuredList = [...featuredList, { id: eat.id, ...eat.data() }])
+          : (list = [...list, { id: eat.id, ...eat.data() }]),
+      )
+
+      if (filter !== 'all') {
+        list = list.filter((eat) => eat.neighborhood === filter)
+      }
+
+      setLoading(false)
+      setFeatured(featuredList)
       setEats(list)
     })
 
     return () => unsubscribe()
-  }, [])
+  }, [filter])
 
   return (
     <div style={styles.container}>
-      {eats.length === 0 && <h3 style={styles.loading}>Loading...</h3>}
+      <div
+        style={{
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'space-between',
+          margin: '0 16px',
+        }}
+      >
+        <p style={{ color: 'grey', fontSize: '15px', marginBottom: -5, marginTop: 0 }}>Featured</p>
+        <button className={styles.filter} onClick={() => toggleFilter(true)}>
+          Filter {filter !== 'all' && <span style={{ fontWeight: 'bold' }}>- {filter}</span>}
+        </button>
+      </div>
+      {loading && <h3 style={styles.loading}>Loading...</h3>}
+      {featured.length > 0 && (
+        <ul style={styles.list}>
+          {featured.map((eat) => (
+            <EatSummary eat={eat} key={eat.id} />
+          ))}
+          <hr style={{ width: '90%', margin: '25px auto' }} />
+        </ul>
+      )}
+      {eats.length === 0 && !loading && <h3 style={styles.loading}>No results in {filter}</h3>}
       {eats.length > 0 && (
         <ul style={styles.list}>
-          <p style={{ color: 'grey', fontSize: '15px', marginBottom: -5, marginLeft: 10 }}>
-            Featured
-          </p>
-          {eats
-            .filter((eat) => eat.featured)
-            .map((eat) => {
-              return <EatSummary eat={eat} key={eat.id} />
-            })}
-          <hr style={{ width: '90%', margin: '25px auto' }} />
-          {eats
-            .filter((eat) => !eat.featured)
-            .map((eat) => {
-              return <EatSummary eat={eat} key={eat.id} />
-            })}
+          {eats.map((eat) => (
+            <EatSummary eat={eat} key={eat.id} />
+          ))}
         </ul>
+      )}
+      {showFilter && (
+        <FilterModal
+          filter={filter}
+          handleClose={() => toggleFilter(false)}
+          handleSubmit={(filter) => {
+            updateFilter(filter)
+            toggleFilter(false)
+          }}
+          title="Filter Eats"
+        />
       )}
     </div>
   )
@@ -45,6 +86,18 @@ export const EatView = () => {
 const getStyles = () => {
   return {
     container: {},
+    filter: css({
+      background: 'white',
+      border: '2px solid black',
+      borderRadius: '5px',
+      boxShadow: 'rgba(87, 102, 117, 0.2) 3px 3px 3px',
+      color: 'black',
+      cursor: 'pointer',
+      padding: '5px',
+      ':hover': {
+        fontWeight: 'bold',
+      },
+    }),
     loading: {
       marginTop: '80px',
       textAlign: 'center',
