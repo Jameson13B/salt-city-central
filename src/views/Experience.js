b0d43d9c784731a6a423c92aac4497cf4e762bc1
Jameson.Brown <jameson.brown@mx.com>
2020-10-07 21:46:20 -0600

    Updated the experience view to include list of summaries

--- src/views/Experience.js
+++ src/views/Experience.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { experiences as experiencesAPI } from '../firebase'
+import { ExperienceSummary } from '../components/experience/ExperienceSummary'
 
 export const Experience = () => {
+  const [experiences, setExperiences] = useState([])
   const styles = getStyles()
 
+  useEffect(() => {
+    let unsubscribe = experiencesAPI((res) => {
+      let list = []
+
+      res.forEach((eat) => (list = [...list, { id: eat.id, ...eat.data() }]))
+      setExperiences(list)
+    })
+
+    return () => unsubscribe()
+  }, [])
+
   return (
-    <div style={styles.experience}>
-      <h2>Experience</h2>
+    <div style={styles.container}>
+      {experiences.length === 0 && <h3 style={styles.loading}>Loading...</h3>}
+      {experiences.length > 0 && (
+        <ul style={styles.list}>
+          <p style={{ color: 'grey', fontSize: '15px', marginBottom: -5, marginLeft: 10 }}>
+            Featured
+          </p>
+          {experiences
+            .filter((experience) => experience.featured)
+            .map((experience) => {
+              return <ExperienceSummary experience={experience} key={experience.id} />
+            })}
+          <hr style={{ width: '90%', margin: '25px auto' }} />
+          {experiences
+            .filter((experience) => !experience.featured)
+            .map((experience) => {
+              return <ExperienceSummary experience={experience} key={experience.id} />
+            })}
+        </ul>
+      )}
     </div>
   )
 }
